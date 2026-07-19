742c39dce30906e2c60397c7faba819a259fb892
Quentin BONHOURE <bonhoure.quentin@gmail.com>
2026-07-19 19:40:06 +0200

    [ADD] Ajout d'un système de deconnexion si un admin place son compte en inactif

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\CheckUserActive;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias(['admin' => AdminMiddleware::class]);
+        $middleware->web(append: [CheckUserActive::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
