f7774c31c950cf6811dc53da58b3e530b6e7229a
Quentin BONHOURE <bonhoure.quentin@gmail.com>
2026-07-19 19:40:06 +0200

    [ADD] Ajout d'un système de deconnexion si un admin place son compte en inactif

---
+++ src/app/Http/Middleware/CheckUserActive.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserActive
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && !Auth::user()->is_active) {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+            return redirect()->route('login')->with('error', 'Votre compte a été désactivé par un administrateur.');
+        }
+        return $next($request);
+    }
+}
